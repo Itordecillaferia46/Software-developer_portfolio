640690cb100ac8b4df5937b10977c8970e082410
unknown <Itordecillaferia46@correo.unicordoba.edu.co>
2025-10-21 00:08:04 -0500

    Configurando skill

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,6 +2,7 @@ import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import meter4 from "../assets/img/meter4.svg";
+import meter5 from "../assets/img/meter5.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
@@ -40,19 +41,23 @@ export const Skills = () => {
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>React</h5>
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>Node</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>JavaScript</h5>
                             </div>
                             <div className="item">
                                 <img src={meter4} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Java</h5>
+                            </div>
+                                <div className="item">
+                                <img src={meter5} alt="Image" />
+                                <h5>Python</h5>
                             </div>
                         </Carousel>
                     </div>
